26e966df7b7cca140a221d30664c1951f132d6a3
jbennyaustria <12301175@students.koi.edu.au>
2024-12-12 22:15:37 +1100

    Add files via upload

---
+++ about/about.js
@@ -0,0 +1,24 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
+
+// Select the navbar element
+const navbar = document.querySelector('.navbar');
+
+// Add an event listener for the scroll event
+window.addEventListener('scroll', () => {
+    // Check the vertical scroll position
+    if (window.scrollY > 50) {
+        // Add the 'scrolled' class to the navbar
+        navbar.classList.add('scrolled');
+    } else {
+        // Remove the 'scrolled' class from the navbar
+        navbar.classList.remove('scrolled');
+    }
+});
